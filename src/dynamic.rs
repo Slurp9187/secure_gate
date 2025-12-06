5d328e26f7500fe4ee13970843fbd5710ac3f916
Slurp9187 <Slurp9187@users.noreply.github.com>
2025-12-06 12:19:48 -0700

    v0.6.0 overhaul

--- src/dynamic.rs
+++ src/dynamic.rs
@@ -1,14 +1,9 @@
-// ==========================================================================
 // src/dynamic.rs
-// ==========================================================================
-
-
 extern crate alloc;
 
 use alloc::boxed::Box;
-use core::ops::{Deref, DerefMut};
 
-pub struct Dynamic<T: ?Sized>(pub Box<T>);
+pub struct Dynamic<T: ?Sized>(Box<T>);
 
 impl<T: ?Sized> Dynamic<T> {
     #[inline(always)]
@@ -23,30 +18,7 @@ impl<T: ?Sized> Dynamic<T> {
     {
         Dynamic(value.into())
     }
-}
 
-impl<T: ?Sized> Deref for Dynamic<T> {
-    type Target = T;
-    #[inline(always)]
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-
-impl<T: ?Sized> DerefMut for Dynamic<T> {
-    #[inline(always)]
-    fn deref_mut(&mut self) -> &mut T {
-        &mut self.0
-    }
-}
-
-impl<T: ?Sized> core::fmt::Debug for Dynamic<T> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str("[REDACTED]")
-    }
-}
-
-impl<T: ?Sized> Dynamic<T> {
     #[inline(always)]
     pub fn expose_secret(&self) -> &T {
         &self.0
@@ -57,28 +29,25 @@ impl<T: ?Sized> Dynamic<T> {
         &mut self.0
     }
 
-    #[deprecated(since = "0.5.5", note = "use `expose_secret` instead")]
-    #[doc(hidden)]
-    #[inline(always)]
-    pub fn view(&self) -> &T {
-        self.expose_secret()
-    }
-
-    #[deprecated(since = "0.5.5", note = "use `expose_secret_mut` instead")]
-    #[doc(hidden)]
-    #[inline(always)]
-    pub fn view_mut(&mut self) -> &mut T {
-        self.expose_secret_mut()
-    }
-
     #[inline(always)]
     pub fn into_inner(self) -> Box<T> {
         self.0
     }
+
+    #[inline(always)]
+    pub fn no_clone(self) -> crate::DynamicNoClone<T> {
+        crate::DynamicNoClone::new(self.0)
+    }
+}
+
+impl<T: ?Sized> core::fmt::Debug for Dynamic<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_str("[REDACTED]")
+    }
 }
 
 #[cfg(not(feature = "zeroize"))]
-impl<T: Clone> Clone for Dynamic<T> {
+impl<T: Clone + ?Sized> Clone for Dynamic<T> {
     #[inline(always)]
     fn clone(&self) -> Self {
         Dynamic(self.0.clone())
@@ -95,7 +64,7 @@ impl<T: Clone + zeroize::Zeroize> Clone for Dynamic<T> {
 
 impl Dynamic<String> {
     pub fn finish_mut(&mut self) -> &mut String {
-        let s = &mut **self;
+        let s = &mut *self.0;
         s.shrink_to_fit();
         s
     }
@@ -103,16 +72,17 @@ impl Dynamic<String> {
 
 impl Dynamic<Vec<u8>> {
     pub fn finish_mut(&mut self) -> &mut Vec<u8> {
-        let v = &mut **self;
+        let v = &mut *self.0;
         v.shrink_to_fit();
         v
     }
+
+    pub fn as_slice(&self) -> &[u8] {
+        self.expose_secret()
+    }
 }
 
-impl<T> From<T> for Dynamic<T>
-where
-    T: Sized,
-{
+impl<T> From<T> for Dynamic<T> {
     #[inline(always)]
     fn from(value: T) -> Self {
         Self(Box::new(value))
@@ -136,15 +106,18 @@ impl From<&str> for Dynamic<String> {
 impl<T: PartialEq + ?Sized> PartialEq for Dynamic<T> {
     #[inline(always)]
     fn eq(&self, other: &Self) -> bool {
-        **self == **other
+        *self.0 == *other.0
     }
 }
 
 impl<T: Eq + ?Sized> Eq for Dynamic<T> {}
 
-impl<T> Dynamic<T> {
-    #[inline(always)]
-    pub fn no_clone(self) -> crate::DynamicNoClone<T> {
-        crate::DynamicNoClone::new(self.0)
+#[cfg(feature = "zeroize")]
+impl<T: ?Sized + zeroize::Zeroize> zeroize::Zeroize for Dynamic<T> {
+    fn zeroize(&mut self) {
+        self.0.zeroize();
     }
 }
+
+#[cfg(feature = "zeroize")]
+impl<T: ?Sized + zeroize::Zeroize> zeroize::ZeroizeOnDrop for Dynamic<T> {}
--- src/fixed.rs
+++ src/fixed.rs
@@ -1,9 +1,5 @@
-// ==========================================================================
 // src/fixed.rs
-// ==========================================================================
-
-
-use core::convert::From;
+use core::fmt;
 use core::ops::{Deref, DerefMut};
 
 pub struct Fixed<T>(pub T);
@@ -12,6 +8,26 @@ impl<T> Fixed<T> {
     #[inline(always)]
     pub const fn new(value: T) -> Self {
         Fixed(value)
+    }
+
+    #[inline(always)]
+    pub fn expose_secret(&self) -> &T {
+        &self.0
+    }
+
+    #[inline(always)]
+    pub fn expose_secret_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+
+    #[inline(always)]
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+
+    #[inline(always)]
+    pub fn no_clone(self) -> crate::FixedNoClone<T> {
+        crate::FixedNoClone::new(self.0)
     }
 }
 
@@ -50,58 +66,20 @@ impl<const N: usize> From<[u8; N]> for Fixed<[u8; N]> {
 impl<const N: usize> AsRef<[u8]> for Fixed<[u8; N]> {
     #[inline(always)]
     fn as_ref(&self) -> &[u8] {
-        &self.0
+        self.expose_secret()
     }
 }
 
 impl<const N: usize> AsMut<[u8]> for Fixed<[u8; N]> {
     #[inline(always)]
     fn as_mut(&mut self) -> &mut [u8] {
-        &mut self.0
-    }
-}
-
-impl<T> core::fmt::Debug for Fixed<T> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str("[REDACTED]")
-    }
-}
-
-impl<T> Fixed<T> {
-    #[inline(always)]
-    pub fn expose_secret(&self) -> &T {
-        &self.0
-    }
-
-    #[inline(always)]
-    pub fn expose_secret_mut(&mut self) -> &mut T {
-        &mut self.0
-    }
-
-    #[deprecated(since = "0.5.5", note = "use `expose_secret` instead")]
-    #[doc(hidden)]
-    #[inline(always)]
-    pub fn view(&self) -> &T {
-        self.expose_secret()
-    }
-
-    #[deprecated(since = "0.5.5", note = "use `expose_secret_mut` instead")]
-    #[doc(hidden)]
-    #[inline(always)]
-    pub fn view_mut(&mut self) -> &mut T {
         self.expose_secret_mut()
     }
-
-    #[inline(always)]
-    pub fn into_inner(self) -> T {
-        self.0
-    }
 }
 
-impl<T> Fixed<T> {
-    #[inline(always)]
-    pub fn no_clone(self) -> crate::FixedNoClone<T> {
-        crate::FixedNoClone::new(self.0)
+impl<T> fmt::Debug for Fixed<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("[REDACTED]")
     }
 }
 
@@ -113,3 +91,13 @@ impl<T: Clone> Clone for Fixed<T> {
 }
 
 impl<const N: usize> Copy for Fixed<[u8; N]> where [u8; N]: Copy {}
+
+#[cfg(feature = "zeroize")]
+impl<T: zeroize::Zeroize> zeroize::Zeroize for Fixed<T> {
+    fn zeroize(&mut self) {
+        self.0.zeroize();
+    }
+}
+
+#[cfg(feature = "zeroize")]
+impl<T: zeroize::Zeroize> zeroize::ZeroizeOnDrop for Fixed<T> {}
